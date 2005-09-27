43027ffc2b09b567fc3992eb85bab86d6809993f
Stefan Roese <sr@denx.de>
2005-09-27 18:42:17 +0200

    [PATCH] ppc32: testversion for AMCC PPC40x mtd support
    This is a testversion for the mtd support with U-Boot partitioning
    for the AMCC PPC40x eval boards (Walnut, Sycamore and Bubinga).
    
    Signed-off-by: Stefan Roese <sr@denx.de>

--- arch/ppc/platforms/4xx/bubinga.h
+++ arch/ppc/platforms/4xx/bubinga.h
@@ -35,7 +35,14 @@
  */
 #define BASE_BAUD		0
 
-#define BUBINGA_FPGA_BASE	0xF0300000
+/* Flash */
+#define PPC40x_FPGA_BASE		0xF0300000
+#define PPC40x_FPGA_REG_OFFS		1	/* offset to flash map reg */
+#define PPC40x_FLASH_ONBD_N(x)		(x & 0x02)
+#define PPC40x_FLASH_SRAM_SEL(x)	(x & 0x01)
+#define PPC40x_FLASH_LOW		0xFFF00000
+#define PPC40x_FLASH_HIGH		0xFFF80000
+#define PPC40x_FLASH_SIZE		0x80000
 
 #define PPC4xx_MACHINE_NAME	"IBM Bubinga"
 
