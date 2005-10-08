17c7aa122929c728b236f9c753870fbb35f800b2
Wolfgang Denk <wd@pollux.denx.de>
2005-10-08 22:35:40 +0200

    Add Andrey Volkov's MPC5200 BestComm/FEC platform drivers
    See: http://patchwork.ozlabs.org/linuxppc/patch?id=2017
         http://patchwork.ozlabs.org/linuxppc//patchcontent?id=2017
    
    Subject: [00/02] MPC5200 Bestcomm platform driver
    From: Andrey Volkov <avolkov@varma-el.com>
    Date: Mon, 15 Aug 2005 14:35:00 +0400
    To: Sylvain Munaut <tnt@246tnt.com>
    Cc: linuxppc-embedded@ozlabs.org

--- include/asm-ppc/mpc52xx.h
+++ include/asm-ppc/mpc52xx.h
@@ -34,6 +34,8 @@ struct pt_regs;
 /* ======================================================================== */
 
 enum ppc_sys_devices {
+	MPC52xx_SDMA,				 /* Must be first device to probe, 
+									else FEC/ATA will failed */
 	MPC52xx_MSCAN1,
 	MPC52xx_MSCAN2,
 	MPC52xx_SPI,
@@ -49,6 +51,7 @@ enum ppc_sys_devices {
 	MPC52xx_ATA,
 	MPC52xx_I2C1,
 	MPC52xx_I2C2,
+	MPC52xx_NUM_DEVICES
 };
 
 
