1c56b1c71e73a5d8d6827f84c34837548d07f8bb
Philippe Gerum <rpm@xenomai.org>
2007-02-15 18:01:22 +0100

    ipipe-2.6.20-i386-1.7-02

--- include/asm-i386/ipipe.h
+++ include/asm-i386/ipipe.h
@@ -27,10 +27,10 @@
 #include <linux/threads.h>
 #include <irq_vectors.h>
 
-#define IPIPE_ARCH_STRING	"1.7-01"
+#define IPIPE_ARCH_STRING	"1.7-02"
 #define IPIPE_MAJOR_NUMBER	1
 #define IPIPE_MINOR_NUMBER	7
-#define IPIPE_PATCH_NUMBER	1
+#define IPIPE_PATCH_NUMBER	2
 
 #ifdef CONFIG_X86_LOCAL_APIC
 /* We want to cover the whole IRQ space when the APIC is enabled. */
