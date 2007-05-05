f735b26079b5dfa0b369a4131a9172f96b6d77a0
Philippe Gerum <rpm@xenomai.org>
2007-05-05 19:09:54 +0200

    ipipe-2.6.20-i386-1.8-00

--- include/asm-i386/ipipe.h
+++ include/asm-i386/ipipe.h
@@ -27,10 +27,10 @@
 #include <linux/threads.h>
 #include <irq_vectors.h>
 
-#define IPIPE_ARCH_STRING	"1.7-03"
+#define IPIPE_ARCH_STRING	"1.8-00"
 #define IPIPE_MAJOR_NUMBER	1
-#define IPIPE_MINOR_NUMBER	7
-#define IPIPE_PATCH_NUMBER	3
+#define IPIPE_MINOR_NUMBER	8
+#define IPIPE_PATCH_NUMBER	0
 
 #ifdef CONFIG_X86_LOCAL_APIC
 /* System interrupts are mapped beyond the last defined external IRQ
