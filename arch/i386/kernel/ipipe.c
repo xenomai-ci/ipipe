e7b140c69794521fe8979a39337f36112dbe330c
Jan Kiszka <jan.kiszka@siemens.com>
2007-10-15 10:25:42 +0200

    Catch unhandled faults over non-root domains

--- arch/i386/kernel/ipipe.c
+++ arch/i386/kernel/ipipe.c
@@ -642,8 +642,17 @@ fastcall int __ipipe_handle_exception(struct pt_regs *regs, long error_code, int
 #endif /* CONFIG_KGDB */
 
 	if (!ipipe_trap_notify(vector, regs)) {
-		__ipipe_exptr handler = __ipipe_std_extable[vector];
-		handler(regs,error_code);
+#ifdef CONFIG_IPIPE_DEBUG
+		if (!ipipe_root_domain_p) {
+			/* Fix up domain so that Linux can handle this. */
+			ipipe_current_domain = ipipe_root_domain;
+			ipipe_trace_panic_freeze();
+			printk(KERN_ERR "BUG: Unhandled exception over domain"
+					" %s - switching to ROOT\n",
+					ipipe_current_domain->name);
+		}
+#endif /* CONFIG_IPIPE_DEBUG */
+		__ipipe_std_extable[vector](regs, error_code);
 		local_irq_restore(flags);
 		__fixup_if(regs);
 		return 0;
