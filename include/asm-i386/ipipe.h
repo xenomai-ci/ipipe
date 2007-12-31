630144def1ae0e8d83a5f989ceb5a218f5784212
Philippe Gerum <rpm@xenomai.org>
2007-12-31 12:28:45 +0100

    Use ipipe_irq_handler_t and discard local placeholder

--- include/asm-i386/ipipe.h
+++ include/asm-i386/ipipe.h
@@ -120,11 +120,8 @@ static inline unsigned long __ipipe_ffnz(unsigned long ul)
 	return ul;
 }
 
-typedef void (__ipipe_irq_handler)(unsigned irq,
-				   void *cookie);
-
 static inline void __ipipe_call_root_xirq_handler(unsigned irq,
-						  __ipipe_irq_handler *handler)
+						  ipipe_irq_handler_t *handler)
 {
 	struct pt_regs *regs = &__raw_get_cpu_var(__ipipe_tick_regs);
 
@@ -155,7 +152,7 @@ static inline void __ipipe_call_root_xirq_handler(unsigned irq,
 void irq_exit(void);
 
 static inline void __ipipe_call_root_virq_handler(unsigned irq,
-						  __ipipe_irq_handler *handler,
+						  ipipe_irq_handler_t *handler,
 						  void *cookie)
 {
 	__asm__ __volatile__("pushfl\n\t"
