5c932f46ae8d4332996c800b58dac70f302b2ee3
Philippe Gerum <rpm@xenomai.org>
2007-12-31 18:58:40 +0100

    ipipe-2.6.24-rc6-x86-2.00-01

--- include/asm-x86/ipipe.h
+++ include/asm-x86/ipipe.h
@@ -1,8 +1,8 @@
 #if defined(CONFIG_IPIPE) && !defined(IPIPE_ARCH_STRING)
-#define IPIPE_ARCH_STRING	"2.0-00"
+#define IPIPE_ARCH_STRING	"2.0-01"
 #define IPIPE_MAJOR_NUMBER	2
 #define IPIPE_MINOR_NUMBER	0
-#define IPIPE_PATCH_NUMBER	0
+#define IPIPE_PATCH_NUMBER	1
 #endif
 #ifdef CONFIG_X86_32
 # include "ipipe_32.h"
