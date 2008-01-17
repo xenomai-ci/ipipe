1d11f4cc94736b387a9859e2819dc96bf3e30431
Philippe Gerum <rpm@xenomai.org>
2008-01-17 17:05:42 +0100

    ipipe-2.6.23-ppc-1.6-03

--- include/asm-ppc/ipipe.h
+++ include/asm-ppc/ipipe.h
@@ -37,10 +37,10 @@
 #include <linux/cache.h>
 #include <linux/threads.h>
 
-#define IPIPE_ARCH_STRING	"1.6-02"
+#define IPIPE_ARCH_STRING	"1.6-03"
 #define IPIPE_MAJOR_NUMBER	1
 #define IPIPE_MINOR_NUMBER	6
-#define IPIPE_PATCH_NUMBER	2
+#define IPIPE_PATCH_NUMBER	3
 
 #define prepare_arch_switch(next)			\
 	do {						\
