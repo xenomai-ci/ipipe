869c861e200d01a7d2364d0b99669d609e0fc729
Philippe Gerum <rpm@xenomai.org>
2008-02-08 15:47:27 +0100

    Fixup the virtual interrupt mask manually when taking the fast exception exit path over non-root domains

--- arch/x86/kernel/ipipe.c
+++ arch/x86/kernel/ipipe.c
@@ -750,13 +750,17 @@ int __ipipe_handle_exception(struct pt_regs *regs, long error_code, int vector)
 	if (!ipipe_root_domain_p &&
 	    __ipipe_xlate_signo[vector] >= 0 &&
 	    !kgdb_handle_exception(vector, __ipipe_xlate_signo[vector], error_code, regs)) {
-		local_irq_restore(flags);
+		if (!flags)
+			__clear_bit(IPIPE_STALL_FLAG,
+				    &ipipe_root_cpudom_var(status));
 		return 1;
 	}
 #endif /* CONFIG_KGDB */
 
 	if (unlikely(ipipe_trap_notify(vector, regs))) {
-		local_irq_restore(flags);
+		if (!flags)
+			__clear_bit(IPIPE_STALL_FLAG,
+				    &ipipe_root_cpudom_var(status));
 		return 1;
 	}
 
