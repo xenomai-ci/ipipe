ca090fdf62fef58b0c45550f43e052a8e5487175
Gilles Chanteperdrix <gilles.chanteperdrix@gmail.com>
2008-05-20 23:58:58 +0200

    Treat non-writable private mappings as shared mappings

--- mm/memory.c
+++ mm/memory.c
@@ -2801,7 +2801,7 @@ static inline int ipipe_pin_pte_range(struct mm_struct *mm, pmd_t *pmd,
 		if (!pte)
 			continue;
 
-		if (!pte_present(*pte)) {
+		if (!pte_present(*pte) || pte_write(*pte)) {
 			pte_unmap_unlock(pte, ptl);
 			continue;
 		}
@@ -2865,7 +2865,8 @@ int ipipe_disable_ondemand_mappings(struct task_struct *tsk)
 		goto done_mm;
 
 	for (vma = mm->mmap; vma; vma = vma->vm_next) {
-		if (!is_cow_mapping(vma->vm_flags))
+		if (!is_cow_mapping(vma->vm_flags)
+		    || !(vma->vm_flags & VM_WRITE))
 			continue;
 
 		addr = vma->vm_start;
