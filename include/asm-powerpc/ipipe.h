33da9ee04e8f27397ea41da7c68a9370dede04ca
Philippe Gerum <rpm@xenomai.org>
2008-06-08 15:10:29 +0200

    ipipe-2.6.25-powerpc-2.2-02

--- include/asm-powerpc/ipipe.h
+++ include/asm-powerpc/ipipe.h
@@ -44,10 +44,10 @@
 #include <asm/paca.h>
 #endif
 
-#define IPIPE_ARCH_STRING	"2.2-01"
+#define IPIPE_ARCH_STRING	"2.2-02"
 #define IPIPE_MAJOR_NUMBER	2
 #define IPIPE_MINOR_NUMBER	2
-#define IPIPE_PATCH_NUMBER	1
+#define IPIPE_PATCH_NUMBER	2
 
 #define prepare_arch_switch(next)			\
 	do {						\
