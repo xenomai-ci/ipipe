87412a27f96d3bd539b470062dd7bc9e291f292b
Philippe Gerum <rpm@xenomai.org>
2008-06-08 09:55:58 +0200

    ipipe-2.6.20-powerpc-1.8-01

--- include/asm-powerpc/ipipe.h
+++ include/asm-powerpc/ipipe.h
@@ -37,10 +37,10 @@
 #include <linux/cache.h>
 #include <linux/threads.h>
 
-#define IPIPE_ARCH_STRING	"1.8-00"
+#define IPIPE_ARCH_STRING	"1.8-01"
 #define IPIPE_MAJOR_NUMBER	1
 #define IPIPE_MINOR_NUMBER	8
-#define IPIPE_PATCH_NUMBER	0
+#define IPIPE_PATCH_NUMBER	1
 
 #define prepare_arch_switch(next)			\
 	do {						\
