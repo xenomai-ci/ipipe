92764c0f2bf4cf5efa8b19d2577fd35af9eb1a70
Philippe Gerum <rpm@xenomai.org>
2008-06-16 18:12:40 +0200

    Use raw_smp_processor_id() in __switch_to

--- arch/x86/kernel/process_32.c
+++ arch/x86/kernel/process_32.c
@@ -655,7 +655,7 @@ struct task_struct * __switch_to(struct task_struct *prev_p, struct task_struct 
 {
 	struct thread_struct *prev = &prev_p->thread,
 				 *next = &next_p->thread;
-	int cpu = smp_processor_id();
+	int cpu = raw_processor_id();
 	struct tss_struct *tss = &per_cpu(init_tss, cpu);
 
 	/* never put a printk in __switch_to... printk() calls wake_up*() indirectly */
