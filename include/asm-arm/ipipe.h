c2e01aef5ebde9c98181b421e93b02462c04acd2
Philippe Gerum <rpm@xenomai.org>
2008-10-01 13:40:06 +0200

    ipipe-2.6.26-arm-1.10-03

--- include/asm-arm/ipipe.h
+++ include/asm-arm/ipipe.h
@@ -28,10 +28,10 @@
 
 #include <linux/ipipe_percpu.h>
 
-#define IPIPE_ARCH_STRING	"1.10-02"
+#define IPIPE_ARCH_STRING	"1.10-03"
 #define IPIPE_MAJOR_NUMBER	1
 #define IPIPE_MINOR_NUMBER	10
-#define IPIPE_PATCH_NUMBER	2
+#define IPIPE_PATCH_NUMBER	3
 
 #ifdef CONFIG_SMP
 #error "I-pipe/arm: SMP not yet implemented"
