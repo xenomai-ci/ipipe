4309eb5fc968a61a9ae3a32d1fb3d4878c7921c6
Gilles Chanteperdrix <gilles.chanteperdrix@xenomai.org>
2009-01-08 17:24:41 +0100

    I-pipe MXC support
    Signed-off-by: Gilles Chanteperdrix <gilles.chanteperdrix@xenomai.org>

--- arch/arm/plat-mxc/include/mach/irqs.h
+++ arch/arm/plat-mxc/include/mach/irqs.h
@@ -13,4 +13,14 @@
 
 #include <mach/hardware.h>
 
+#ifdef CONFIG_IPIPE
+#ifdef CONFIG_ARCH_MX3
+#define __ipipe_mach_irq_mux_p(irq)				\
+	((irq) == MXC_INT_GPIO1					\
+	 || (irq) == MXC_INT_GPIO2 || (irq) == MXC_INT_GPIO3)
+#elif CONFIG_ARCH_MX2
+#define __ipipe_mach_irq_mux_p(irq) ((irq) == MXC_INT_GPIO)
+#endif /* CONFIG_ARCH_MX2 */
+#endif /* CONFIG_IPIPE */
+
 #endif /* __ASM_ARCH_MXC_IRQS_H__ */
