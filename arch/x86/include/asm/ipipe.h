bb43480efcdc2d5368da9d6710eb649d62b38740
Philippe Gerum <rpm@xenomai.org>
2009-04-10 10:28:36 +0200

    ipipe-2.6.29.1-x86-2.3-00

--- arch/x86/include/asm/ipipe.h
+++ arch/x86/include/asm/ipipe.h
@@ -25,10 +25,10 @@
 #ifdef CONFIG_IPIPE
 
 #ifndef IPIPE_ARCH_STRING
-#define IPIPE_ARCH_STRING	"2.2-06"
+#define IPIPE_ARCH_STRING	"2.3-00"
 #define IPIPE_MAJOR_NUMBER	2
-#define IPIPE_MINOR_NUMBER	2
-#define IPIPE_PATCH_NUMBER	6
+#define IPIPE_MINOR_NUMBER	3
+#define IPIPE_PATCH_NUMBER	0
 #endif
 
 DECLARE_PER_CPU(struct pt_regs, __ipipe_tick_regs);
