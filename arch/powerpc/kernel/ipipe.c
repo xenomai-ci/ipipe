a828f73a18e9f5921b3b9589f4573b5416ee6d00
Philippe Gerum <rpm@xenomai.org>
2009-05-24 23:54:27 +0200

    Fix preemption state to prevent recursive preemption call

--- arch/powerpc/kernel/ipipe.c
+++ arch/powerpc/kernel/ipipe.c
@@ -737,8 +737,10 @@ void __sched  __ipipe_preempt_schedule_irq(void)
 	 */
 	p = ipipe_root_cpudom_ptr(); 
 	if (unlikely(p->irqpend_himask != 0)) { 
+		add_preempt_count(PREEMPT_ACTIVE);
 		clear_bit(IPIPE_STALL_FLAG, &p->status); 
 		__ipipe_sync_pipeline(IPIPE_IRQMASK_ANY); 
+		sub_preempt_count(PREEMPT_ACTIVE);
 	} 
 
 	__local_irq_restore_nosync(flags);  
