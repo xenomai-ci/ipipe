5d61fe95ab23a78ba27f3a688ff0e70c60e18b13
Philippe Gerum <rpm@xenomai.org>
2009-10-04 12:18:35 +0200

    Intercept IRQ_MOVE_CLEANUP vector

--- arch/x86/kernel/ipipe.c
+++ arch/x86/kernel/ipipe.c
@@ -273,6 +273,13 @@ void __init __ipipe_enable_pipeline(void)
 			     NULL,
 			     &__ipipe_ack_apic,
 			     IPIPE_STDROOT_MASK);
+
+	ipipe_virtualize_irq(ipipe_root_domain,
+			     ipipe_apic_vector_irq(IRQ_MOVE_CLEANUP_VECTOR),
+			     (ipipe_irq_handler_t)&smp_irq_move_cleanup_interrupt,
+			     NULL,
+			     &__ipipe_ack_apic,
+			     IPIPE_STDROOT_MASK);
 #else
 	(void)vector;
 #endif	/* CONFIG_SMP */
