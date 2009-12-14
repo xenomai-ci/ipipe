9508665feb17608178a1c6b1051525d644de2f8f
Jan Kiszka <jan.kiszka@siemens.com>
2009-12-14 18:12:49 +0100

    Disable both real and virtual interrupts during ftrace code changes
    This is required as the call stack contains some further local_irq_save/
    restore which would otherwise reenable real interrupts spuriously.
    
    Signed-off-by: Jan Kiszka <jan.kiszka@siemens.com>

--- kernel/trace/ftrace.c
+++ kernel/trace/ftrace.c
@@ -2802,9 +2802,11 @@ static int ftrace_convert_nops(struct module *mod,
 	}
 
 	/* disable interrupts to prevent kstop machine */
-	local_irq_save_hw_notrace(flags);
+	local_irq_save(flags);
+	local_irq_disable_hw();
 	ftrace_update_code(mod);
-	local_irq_restore_hw_notrace(flags);
+	local_irq_enable_hw();
+	local_irq_restore(flags);
 	mutex_unlock(&ftrace_lock);
 
 	return 0;
@@ -2887,9 +2889,11 @@ void __init ftrace_init(void)
 	/* Keep the ftrace pointer to the stub */
 	addr = (unsigned long)ftrace_stub;
 
-	local_irq_save_hw_notrace(flags);
+	local_irq_save(flags);
+	local_irq_disable_hw();
 	ftrace_dyn_arch_init(&addr);
-	local_irq_restore_hw_notrace(flags);
+	local_irq_enable_hw();
+	local_irq_restore(flags);
 
 	/* ftrace_dyn_arch_init places the return code in addr */
 	if (addr)
