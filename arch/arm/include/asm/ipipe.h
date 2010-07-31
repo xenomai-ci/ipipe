d0946c73a7756b5d7b69d46c75c8ccd76080a827
Gilles Chanteperdrix <gilles.chanteperdrix@xenomai.org>
2010-08-01 01:57:33 +0200

    adeos-ipipe-2.6.31-arm-1.16-02

--- arch/arm/include/asm/ipipe.h
+++ arch/arm/include/asm/ipipe.h
@@ -29,10 +29,10 @@
 #include <linux/ipipe_percpu.h>
 #include <mach/irqs.h>		/* For __IPIPE_FEATURE_PIC_MUTE */
 
-#define IPIPE_ARCH_STRING	"1.16-01"
+#define IPIPE_ARCH_STRING	"1.16-02"
 #define IPIPE_MAJOR_NUMBER	1
 #define IPIPE_MINOR_NUMBER	16
-#define IPIPE_PATCH_NUMBER	1
+#define IPIPE_PATCH_NUMBER	2
 
 #ifdef CONFIG_SMP
 #error "I-pipe/arm: SMP not yet implemented"
