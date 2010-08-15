4c7da246e541bd6bc2ffd6e07009aeed862c08e0
Gilles Chanteperdrix <gilles.chanteperdrix@xenomai.org>
2010-08-15 22:15:07 +0200

    ipipe_sync_pipeline no longer need arguments

--- arch/arm/kernel/ipipe.c
+++ arch/arm/kernel/ipipe.c
@@ -505,7 +505,7 @@ asmlinkage void __sched __ipipe_preempt_schedule_irq(void)
 	if (unlikely(__ipipe_ipending_p(p))) {
 		add_preempt_count(PREEMPT_ACTIVE);
 		clear_bit(IPIPE_STALL_FLAG, &p->status);
-		__ipipe_sync_pipeline(IPIPE_IRQ_DOALL);
+		__ipipe_sync_pipeline();
 		sub_preempt_count(PREEMPT_ACTIVE);
 	}
 
@@ -583,7 +583,7 @@ asmlinkage int __ipipe_syscall_root(unsigned long scno, struct pt_regs *regs)
 	else {
 		p = ipipe_root_cpudom_ptr();
 		if (__ipipe_ipending_p(p))
-			__ipipe_sync_pipeline(IPIPE_IRQ_DOALL);
+			__ipipe_sync_pipeline();
 	}
 
 	local_irq_enable_hw();
