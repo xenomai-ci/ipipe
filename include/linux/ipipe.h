1483e084a6770b6b507b0b92502f21dea1a7373e
Philippe Gerum <rpm@xenomai.org>
2011-02-01 18:29:52 +0100

    ipipe: disable __ipipe_check_root_resched() for non-preemptible kernels

--- include/linux/ipipe.h
+++ include/linux/ipipe.h
@@ -266,8 +266,12 @@ static inline void ipipe_irq_unlock(unsigned irq)
 #endif
 
 #ifndef __ipipe_check_root_resched
+#ifdef CONFIG_PREEMPT
 #define __ipipe_check_root_resched()			\
 	(preempt_count() == 0 && need_resched())
+#else
+#define __ipipe_check_root_resched()  0
+#endif
 #endif
 
 #ifndef __ipipe_run_irqtail
