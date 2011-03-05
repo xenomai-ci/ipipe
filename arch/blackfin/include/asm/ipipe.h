b2ca77326c1ab20c560a39f5560c5275f26299c2
Philippe Gerum <rpm@xenomai.org>
2011-03-05 16:26:08 +0100

    blackfin/ipipe: fix call args to __ipipe_run_irqtail()

--- arch/blackfin/include/asm/ipipe.h
+++ arch/blackfin/include/asm/ipipe.h
@@ -149,7 +149,7 @@ static inline unsigned long __ipipe_ffnz(unsigned long ul)
 #define __ipipe_do_root_xirq(ipd, irq)					\
 	(ipd)->irqs[irq].handler(irq, &__raw_get_cpu_var(__ipipe_tick_regs))
 
-#define __ipipe_run_irqtail()  /* Must be a macro */			\
+#define __ipipe_run_irqtail(irq)  /* Must be a macro */			\
 	do {								\
 		unsigned long __pending;				\
 		CSYNC();						\
