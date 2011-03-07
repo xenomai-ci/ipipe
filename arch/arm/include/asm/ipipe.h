f32a92e845fc0930b11cdde24df096fadc342671
Gilles Chanteperdrix <gilles.chanteperdrix@xenomai.org>
2011-03-07 21:53:48 +0100

    adeos-ipipe-2.6.33-arm-1.18-02

--- arch/arm/include/asm/ipipe.h
+++ arch/arm/include/asm/ipipe.h
@@ -30,10 +30,10 @@
 #include <linux/ipipe_percpu.h>
 #include <linux/ipipe_trace.h>
 
-#define IPIPE_ARCH_STRING	"1.18-01"
+#define IPIPE_ARCH_STRING	"1.18-02"
 #define IPIPE_MAJOR_NUMBER	1
 #define IPIPE_MINOR_NUMBER	18
-#define IPIPE_PATCH_NUMBER	1
+#define IPIPE_PATCH_NUMBER	2
 
 #ifdef CONFIG_SMP
 #define ipipe_processor_id()	hard_smp_processor_id()
