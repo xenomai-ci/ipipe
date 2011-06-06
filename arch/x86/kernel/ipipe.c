08cff920efedd0eaed4e22df9d2d80a8b25d2456
Jan Kiszka <jan.kiszka@siemens.com>
2011-06-06 14:49:05 +0200

    ipipe: x86: Unbreak !CONFIG_SPARSE_IRQ build
    Signed-off-by: Jan Kiszka <jan.kiszka@siemens.com>

--- arch/x86/kernel/ipipe.c
+++ arch/x86/kernel/ipipe.c
@@ -835,7 +835,9 @@ void update_vsyscall_tz(void)
 
 EXPORT_SYMBOL(__ipipe_tick_irq);
 
+#ifdef CONFIG_SPARSE_IRQ
 EXPORT_SYMBOL_GPL(irq_to_desc);
+#endif
 struct task_struct *__switch_to(struct task_struct *prev_p,
 				struct task_struct *next_p);
 EXPORT_SYMBOL_GPL(__switch_to);
