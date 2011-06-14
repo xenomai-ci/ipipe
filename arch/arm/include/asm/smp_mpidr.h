8a52ce8afe2e1b90f572514ac11c042639f1c5f6
Gilles Chanteperdrix <gilles.chanteperdrix@xenomai.org>
2011-06-14 19:52:21 +0200

    ipipe arm: fix hard_smp_processor_id

--- arch/arm/include/asm/smp_mpidr.h
+++ arch/arm/include/asm/smp_mpidr.h
@@ -4,13 +4,13 @@
 #define hard_smp_processor_id()						\
 	({								\
 		unsigned int cpunum;					\
-		__asm__("\n"						\
+		__asm__ __volatile__ ("\n"				\
 			"1:	mrc p15, 0, %0, c0, c0, 5\n"		\
 			"	.pushsection \".alt.smp.init\", \"a\"\n"\
 			"	.long	1b\n"				\
 			"	mov	%0, #0\n"			\
 			"	.popsection"				\
-			: "=r" (cpunum));				\
+				      : "=r" (cpunum));			\
 		cpunum &= 0x0F;						\
 	})
 
