ca415955a2ee0a987d13ccbc9c29d2bef86c34e6
Philippe Gerum <rpm@xenomai.org>
2012-04-25 15:40:31 +0200

    powerpc/ipipe: fix timebase to ns/us conversion

--- arch/powerpc/include/asm/ipipe.h
+++ arch/powerpc/include/asm/ipipe.h
@@ -120,8 +120,8 @@ extern cpumask_t __ipipe_dbrk_pending;
 
 #ifdef CONFIG_PPC64
 #define ipipe_read_tsc(t)	(t = mftb())
-#define ipipe_tsc2ns(t)		(((t) * 1000UL) / (__ipipe_cpu_freq / 1000000UL))
-#define ipipe_tsc2us(t)		((t) / (__ipipe_cpu_freq / 1000000UL))
+#define ipipe_tsc2ns(t)		(((t) * 1000UL) / (__ipipe_hrclock_freq / 1000000UL))
+#define ipipe_tsc2us(t)		((t) / (__ipipe_hrclock_freq / 1000000UL))
 #else /* CONFIG_PPC32 */
 #define ipipe_read_tsc(t)					\
 	({							\
@@ -138,12 +138,12 @@ extern cpumask_t __ipipe_dbrk_pending;
 	})
 
 #define ipipe_tsc2ns(t)	\
-	((((unsigned long)(t)) * 1000) / (__ipipe_cpu_freq / 1000000))
+	((((unsigned long)(t)) * 1000) / (__ipipe_hrclock_freq / 1000000))
 
 #define ipipe_tsc2us(t)						\
 	({							\
 		unsigned long long delta = (t);			\
-		do_div(delta, __ipipe_cpu_freq/1000000+1);	\
+		do_div(delta, __ipipe_hrclock_freq/1000000+1);	\
 		(unsigned long)delta;				\
 	})
 #endif /* CONFIG_PPC32 */
