56f7935d41cb7dd02f3eb013c5f85563c7190bac
Gilles Chanteperdrix <gilles.chanteperdrix@xenomai.org>
2012-07-29 15:40:15 +0200

    ipipe: allow ipi to be handled as virq
    When IPIs are handled as VIRQs, the linux handler should not call
    irq_enter/irq_exit. Define IPIPE_ARCH_HAVE_VIRQ_IPI to be defined by
    the architectures if IPIs are handled as VIRQs, and in scheduler_ipi,
    only call irq_enter/irq_exit if it is not defined.

--- kernel/sched/core.c
+++ kernel/sched/core.c
@@ -1501,7 +1501,9 @@ void scheduler_ipi(void)
 	 * however a fair share of IPIs are still resched only so this would
 	 * somewhat pessimize the simple resched case.
 	 */
+#ifndef IPIPE_ARCH_HAVE_VIRQ_IPI
 	irq_enter();
+#endif
 	sched_ttwu_pending();
 
 	/*
@@ -1511,7 +1513,9 @@ void scheduler_ipi(void)
 		this_rq()->idle_balance = 1;
 		raise_softirq_irqoff(SCHED_SOFTIRQ);
 	}
+#ifndef IPIPE_ARCH_HAVE_VIRQ_IPI
 	irq_exit();
+#endif
 }
 
 static void ttwu_queue_remote(struct task_struct *p, int cpu)
