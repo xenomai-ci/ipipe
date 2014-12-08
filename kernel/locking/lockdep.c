a5d4be10ba41133fb65c9abff6934a2ad6afb78b
Jan Kiszka <jan.kiszka@siemens.com>
2014-12-08 19:48:58 +0100

    ipipe: Do not trace Linux irqs over the head domain
    Filter out any calls to trace_hardirqs* issued over the head domain.
    These may break assumptions of that domain and/or cause significant
    latencies. Specifically assembly entry code may call into these
    functions independent of the current domain.
    
    Signed-off-by: Jan Kiszka <jan.kiszka@siemens.com>

--- kernel/locking/lockdep.c
+++ kernel/locking/lockdep.c
@@ -2572,6 +2572,9 @@ static void __trace_hardirqs_on_caller(unsigned long ip)
 
 __visible void trace_hardirqs_on_caller(unsigned long ip)
 {
+	if (!ipipe_root_p)
+		return;
+
 	time_hardirqs_on(CALLER_ADDR0, ip);
 
 	if (unlikely(!debug_locks || current->lockdep_recursion))
@@ -2625,7 +2628,12 @@ EXPORT_SYMBOL(trace_hardirqs_on);
  */
 __visible void trace_hardirqs_off_caller(unsigned long ip)
 {
-	struct task_struct *curr = current;
+	struct task_struct *curr;
+
+	if (!ipipe_root_p)
+		return;
+
+	curr = current;
 
 	time_hardirqs_off(CALLER_ADDR0, ip);
 
