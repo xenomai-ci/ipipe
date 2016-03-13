4987ad8a41401fad534022d5c05190ccb15c7734
Philippe Gerum <rpm@xenomai.org>
2016-03-13 11:47:25 +0100

    ipipe-core-3.18.20-blackfin-5

--- arch/blackfin/include/asm/ipipe.h
+++ arch/blackfin/include/asm/ipipe.h
@@ -36,7 +36,7 @@
 #include <asm/traps.h>
 #include <asm/bitsperlong.h>
 
-#define IPIPE_CORE_RELEASE	4
+#define IPIPE_CORE_RELEASE	5
 
 #ifdef CONFIG_SMP
 #error "I-pipe/blackfin: SMP not implemented"
