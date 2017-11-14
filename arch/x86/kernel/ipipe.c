xenomai-ci/ipipe

commit 61553d8dcb9f5d9a2f5c59800a97c38beecf9d6a

x86/ipipe: use common path for handling kgdb exceptions
We may detect memory probing calls by testing faulthandler_disabled()
from any domain context.

When a trap is received from a kernel context that probes memory from
the head stage, attempt to fix up the return address immediately over
the pipeline prologue code. This covers the case of KGDB traps, and
more generally any access exception triggered by the
probe_kernel_read/write() routines.

arch/x86/kernel/ipipe.c
@@ -312,23 +312,10 @@ EXPORT_SYMBOL_GPL(__ipipe_halt_root);
 int __ipipe_trap_prologue(struct pt_regs *regs, int trapnr)
 {
 	bool entry_irqs_off = hard_irqs_disabled(),
-		trap_irqs_off = raw_irqs_disabled_flags(regs->flags);
+		trap_irqs_off = raw_irqs_disabled_flags(regs->flags),
+		root_entry = ipipe_root_p;
 	struct ipipe_domain *ipd;
 	unsigned long flags, cr2;
-
-#ifdef CONFIG_KGDB
-	/* Fixup kgdb-own faults immediately. */
-	if (__ipipe_probe_access) {
-		const struct exception_table_entry *fixup =
-			search_exception_tables(regs->ip);
-		BUG_ON(!fixup);
-		regs->ip = (unsigned long)&fixup->fixup + fixup->fixup;
-		return 1;
-	}
-#endif /* CONFIG_KGDB */
-
-	if (trapnr == X86_TRAP_PF)
-		cr2 = native_read_cr2();
 
 	/*
 	 * KGDB and ftrace may poke int3/debug ops into the kernel
@@ -343,17 +330,34 @@ int __ipipe_trap_prologue(struct pt_regs *regs, int trapnr)
 		 * but do call the regular handler which is assumed to
 		 * run fine within such context.
 		 */
-		if (!ipipe_root_p)
+		if (!root_entry)
 			return -1;
 		local_save_flags(flags);
 		__ipipe_fixup_if(raw_irqs_disabled_flags(flags), regs);
 		goto out_root;
 	}
-	
+
+	/*
+	 * Now that we have filtered out all debug traps which might
+	 * happen anywhere in kernel code in theory, detect attempts
+	 * to probe kernel memory (i.e. calls to probe_kernel_{read,
+	 * write}()). If that happened over the head domain, do the
+	 * fixup immediately then return right after upon success. If
+	 * that fails, the kernel is likely to crash but let's follow
+	 * the standard recovery procedure in that case anyway.
+	 */
+	if (unlikely(!root_entry && faulthandler_disabled())) {
+		if (fixup_exception(regs, trapnr))
+			return 1;
+	}
+
+	if (trapnr == X86_TRAP_PF)
+		cr2 = native_read_cr2();
+
 	if (unlikely(__ipipe_notify_trap(trapnr, regs)))
 		return 1;
 
-	if (likely(ipipe_root_p)) {
+	if (likely(root_entry)) {
 		/*
 		 * If no head domain is installed, or in case we faulted in
 		 * the iret path of x86-32, regs->flags does not match the root

arch/x86/kernel/kgdb.c
@@ -755,13 +755,12 @@ int kgdb_arch_set_breakpoint(struct kgdb_bkpt *bpt)
 #endif /* CONFIG_DEBUG_RODATA */
 
 	bpt->type = BP_BREAKPOINT;
-	err = ipipe_probe_kernel_read(bpt->saved_instr, (char *)bpt->bpt_addr,
-				      BREAK_INSTR_SIZE);
+	err = probe_kernel_read(bpt->saved_instr, (char *)bpt->bpt_addr,
+				BREAK_INSTR_SIZE);
 	if (err)
 		return err;
-	err = ipipe_probe_kernel_write((char *)bpt->bpt_addr,
-				       arch_kgdb_ops.gdb_bpt_instr,
-				       BREAK_INSTR_SIZE);
+	err = probe_kernel_write((char *)bpt->bpt_addr,
+				 arch_kgdb_ops.gdb_bpt_instr, BREAK_INSTR_SIZE);
 #ifdef CONFIG_DEBUG_RODATA
 	if (!err)
 		return err;
@@ -773,8 +772,7 @@ int kgdb_arch_set_breakpoint(struct kgdb_bkpt *bpt)
 		return -EBUSY;
 	text_poke((void *)bpt->bpt_addr, arch_kgdb_ops.gdb_bpt_instr,
 		  BREAK_INSTR_SIZE);
-	err = ipipe_probe_kernel_read(opc, (char *)bpt->bpt_addr,
-				      BREAK_INSTR_SIZE);
+	err = probe_kernel_read(opc, (char *)bpt->bpt_addr, BREAK_INSTR_SIZE);
 	if (err)
 		return err;
 	if (memcmp(opc, arch_kgdb_ops.gdb_bpt_instr, BREAK_INSTR_SIZE))
@@ -799,16 +797,14 @@ int kgdb_arch_remove_breakpoint(struct kgdb_bkpt *bpt)
 	if (mutex_is_locked(&text_mutex))
 		goto knl_write;
 	text_poke((void *)bpt->bpt_addr, bpt->saved_instr, BREAK_INSTR_SIZE);
-	err = ipipe_probe_kernel_read(opc, (char *)bpt->bpt_addr,
-				      BREAK_INSTR_SIZE);
+	err = probe_kernel_read(opc, (char *)bpt->bpt_addr, BREAK_INSTR_SIZE);
 	if (err || memcmp(opc, bpt->saved_instr, BREAK_INSTR_SIZE))
 		goto knl_write;
 	return err;
 knl_write:
 #endif /* CONFIG_DEBUG_RODATA */
-	return ipipe_probe_kernel_write((char *)bpt->bpt_addr,
-					(char *)bpt->saved_instr,
-					BREAK_INSTR_SIZE);
+	return probe_kernel_write((char *)bpt->bpt_addr,
+				  (char *)bpt->saved_instr, BREAK_INSTR_SIZE);
 }
 
 struct kgdb_arch arch_kgdb_ops = {

commit 0f8ccc64676cf7f8ca33e1285192555f98670a67

x86/ipipe: fix compile error
61553d8dcb9f introduced a call of "fixup_exception" with too many args,
probably a backporting problem. Remove the arg that the 4.4-version of
the function did not need.

Signed-off-by: Henning Schild <henning.schild@siemens.com>

arch/x86/kernel/ipipe.c
@@ -347,7 +347,7 @@ int __ipipe_trap_prologue(struct pt_regs *regs, int trapnr)
 	 * the standard recovery procedure in that case anyway.
 	 */
 	if (unlikely(!root_entry && faulthandler_disabled())) {
-		if (fixup_exception(regs, trapnr))
+		if (fixup_exception(regs))
 			return 1;
 	}
 