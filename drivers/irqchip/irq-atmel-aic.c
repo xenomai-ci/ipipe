microsoft/WSL2-Linux-Kernel

commit 1a66e50c45e6abb6fc14509a44d2eb210f7f2957

pinctrl: at91: don't use the same irqchip with multiple gpiochips
[ Upstream commit 0c3dfa176912b5f87732545598200fb55e9c1978 ]

Sharing the same irqchip with multiple gpiochips is not a good
practice. For instance, when installing hooks, we change the state
of the irqchip. The initial state of the irqchip for the second
gpiochip to register is then disrupted.

Signed-off-by: Ludovic Desroches <ludovic.desroches@microchip.com>
Signed-off-by: Linus Walleij <linus.walleij@linaro.org>
Signed-off-by: Sasha Levin <sashal@kernel.org>

drivers/pinctrl/pinctrl-at91.c
@@ -1556,16 +1556,6 @@ void at91_pinctrl_gpio_resume(void)
 #define gpio_irq_set_wake	NULL
 #endif /* CONFIG_PM */
 
-static struct irq_chip gpio_irqchip = {
-	.name		= "GPIO",
-	.irq_ack	= gpio_irq_ack,
-	.irq_disable	= gpio_irq_mask,
-	.irq_mask	= gpio_irq_mask,
-	.irq_unmask	= gpio_irq_unmask,
-	/* .irq_set_type is set dynamically */
-	.irq_set_wake	= gpio_irq_set_wake,
-};
-
 static void gpio_irq_handler(struct irq_desc *desc)
 {
 	struct irq_chip *chip = irq_desc_get_chip(desc);
@@ -1608,12 +1598,22 @@ static int at91_gpio_of_irq_setup(struct platform_device *pdev,
 	struct gpio_chip	*gpiochip_prev = NULL;
 	struct at91_gpio_chip   *prev = NULL;
 	struct irq_data		*d = irq_get_irq_data(at91_gpio->pioc_virq);
+	struct irq_chip		*gpio_irqchip;
 	int ret, i;
+
+	gpio_irqchip = devm_kzalloc(&pdev->dev, sizeof(*gpio_irqchip), GFP_KERNEL);
+	if (!gpio_irqchip)
+		return -ENOMEM;
 
 	at91_gpio->pioc_hwirq = irqd_to_hwirq(d);
 
-	/* Setup proper .irq_set_type function */
-	gpio_irqchip.irq_set_type = at91_gpio->ops->irq_type;
+	gpio_irqchip->name = "GPIO";
+	gpio_irqchip->irq_ack = gpio_irq_ack;
+	gpio_irqchip->irq_disable = gpio_irq_mask;
+	gpio_irqchip->irq_mask = gpio_irq_mask;
+	gpio_irqchip->irq_unmask = gpio_irq_unmask;
+	gpio_irqchip->irq_set_wake = gpio_irq_set_wake,
+	gpio_irqchip->irq_set_type = at91_gpio->ops->irq_type;
 
 	/* Disable irqs of this PIO controller */
 	writel_relaxed(~0, at91_gpio->regbase + PIO_IDR);
@@ -1624,7 +1624,7 @@ static int at91_gpio_of_irq_setup(struct platform_device *pdev,
 	 * interrupt.
 	 */
 	ret = gpiochip_irqchip_add(&at91_gpio->chip,
-				   &gpio_irqchip,
+				   gpio_irqchip,
 				   0,
 				   handle_edge_irq,
 				   IRQ_TYPE_EDGE_BOTH);
@@ -1642,7 +1642,7 @@ static int at91_gpio_of_irq_setup(struct platform_device *pdev,
 	if (!gpiochip_prev) {
 		/* Then register the chain on the parent IRQ */
 		gpiochip_set_chained_irqchip(&at91_gpio->chip,
-					     &gpio_irqchip,
+					     gpio_irqchip,
 					     at91_gpio->pioc_virq,
 					     gpio_irq_handler);
 		return 0;

xenomai-ci/ipipe

commit 9ec8a62511b605954b054573f5de632b1a5c991a

arm/ipipe: Remove support for pic muting from atmel-aic[5]
This was broken by 1a66e50c45e6, and there is no simple functional fix
for it. So fix the build at least.

Signed-off-by: Jan Kiszka <jan.kiszka@siemens.com>

drivers/irqchip/irq-atmel-aic.c
@@ -75,14 +75,6 @@ aic_handle(struct pt_regs *regs)
 }
 
 #ifdef CONFIG_IPIPE
-static unsigned aic_root = ~0U;
-static unsigned aic_muted;
-
-int at91_gpio_enable_irqdesc(struct ipipe_domain *ipd, unsigned irq);
-int at91_gpio_disable_irqdesc(struct ipipe_domain *ipd, unsigned irq);
-void at91_gpio_mute(void);
-void at91_gpio_unmute(void);
-
 static void aic_hold(struct irq_data *d)
 {
 	struct irq_chip_generic *gc = irq_data_get_irq_chip_data(d);
@@ -99,73 +91,6 @@ static void aic_release(struct irq_data *d)
 	flags = irq_gc_lock(gc);
 	irq_gc_unmask_enable_reg(d);
 	irq_gc_unlock(gc, flags);
-}
-
-static void at91_enable_irqdesc(struct ipipe_domain *ipd, unsigned irq)
-{
-	int err;
-
-	err = at91_gpio_enable_irqdesc(ipd, irq);
-	if (err < 0) {
-		if (ipd != &ipipe_root) {
-			struct irq_desc *desc = irq_to_desc(irq);
-			struct irq_data *idata = irq_desc_get_irq_data(desc);
-
-			aic_root &= ~(1 << idata->hwirq);
-		}
-	} else if (err)
-		aic_root &= ~(1 << err);
-}
-
-static void at91_disable_irqdesc(struct ipipe_domain *ipd, unsigned irq)
-{
-	int err;
-
-	err = at91_gpio_disable_irqdesc(ipd, irq);
-	if (err < 0) {
-		if (ipd != &ipipe_root) {
-			struct irq_desc *desc = irq_to_desc(irq);
-			struct irq_data *idata = irq_desc_get_irq_data(desc);
-
-			aic_root |= (1 << idata->hwirq);
-		}
-	} else if (err)
-		aic_root |= (1 << err);
-}
-
-static void at91_mute_pic(void)
-{
-	struct irq_domain_chip_generic *dgc = aic_domain->gc;
-	struct irq_chip_generic *gc = dgc->gc[0];
-	unsigned long unmasked, muted;
-
-	at91_gpio_mute();
-
-	unmasked = irq_reg_readl(gc, AT91_AIC_IMR);
-	aic_muted = muted = unmasked & aic_root;
-	irq_reg_writel(gc, muted, AT91_AIC_IDCR);
-}
-
-static void at91_unmute_pic(void)
-{
-	struct irq_domain_chip_generic *dgc = aic_domain->gc;
-	struct irq_chip_generic *gc = dgc->gc[0];
-
-	irq_reg_writel(gc, aic_muted, AT91_AIC_IECR);
-
-	at91_gpio_unmute();
-}
-
-static void at91_pic_muter_register(void)
-{
-	struct ipipe_mach_pic_muter at91_pic_muter = {
-		.enable_irqdesc = at91_enable_irqdesc,
-		.disable_irqdesc = at91_disable_irqdesc,
-		.mute = at91_mute_pic,
-		.unmute = at91_unmute_pic,
-	};
-
-	ipipe_pic_muter_register(&at91_pic_muter);
 }
 #endif
 
@@ -370,7 +295,6 @@ static int __init aic_of_init(struct device_node *node,
 #ifdef CONFIG_IPIPE
 	gc->chip_types[0].chip.irq_hold	= aic_hold;
 	gc->chip_types[0].chip.irq_release = aic_release;
-	at91_pic_muter_register();
 #endif
 
 	aic_hw_init(domain);

drivers/irqchip/irq-atmel-aic5.c
@@ -122,11 +122,6 @@ static void aic5_unmask(struct irq_data *d)
 }
 
 #ifdef CONFIG_IPIPE
-int at91_gpio_enable_irqdesc(struct ipipe_domain *ipd, unsigned irq);
-int at91_gpio_disable_irqdesc(struct ipipe_domain *ipd, unsigned irq);
-void at91_gpio_mute(void);
-void at91_gpio_unmute(void);
-
 static void aic5_hold(struct irq_data *d)
 {
 	struct irq_domain *domain = d->domain;
@@ -149,38 +144,6 @@ static void aic5_release(struct irq_data *d)
 	irq_reg_writel(gc, d->hwirq, AT91_AIC5_SSR);
 	irq_reg_writel(gc, 1, AT91_AIC5_IECR);
 	irq_gc_unlock(gc, flags);
-}
-
-static void at91_enable_irqdesc(struct ipipe_domain *ipd, unsigned irq)
-{
-	at91_gpio_enable_irqdesc(ipd, irq);
-}
-
-static void at91_disable_irqdesc(struct ipipe_domain *ipd, unsigned irq)
-{
-	at91_gpio_disable_irqdesc(ipd, irq);
-}
-
-static void at91_mute_pic(void)
-{
-	at91_gpio_mute();
-}
-
-static void at91_unmute_pic(void)
-{
-	at91_gpio_unmute();
-}
-
-static void at91_pic_muter_register(void)
-{
-	struct ipipe_mach_pic_muter at91_pic_muter = {
-		.enable_irqdesc = at91_enable_irqdesc,
-		.disable_irqdesc = at91_disable_irqdesc,
-		.mute = at91_mute_pic,
-		.unmute = at91_unmute_pic,
-	};
-
-	ipipe_pic_muter_register(&at91_pic_muter);
 }
 #endif
 
@@ -412,10 +375,6 @@ static int __init aic5_of_init(struct device_node *node,
 
 	aic5_hw_init(domain);
 	set_handle_irq(aic5_handle);
-
-#ifdef CONFIG_IPIPE
-	at91_pic_muter_register();
-#endif
 
 	return 0;
 }

drivers/pinctrl/pinctrl-at91.c
@@ -1826,96 +1826,6 @@ err:
 	return ret;
 }
 
-#ifdef CONFIG_IPIPE
-int at91_gpio_enable_irqdesc(struct ipipe_domain *ipd, unsigned irq)
-{
-	struct at91_gpio_chip *at91_chip;
-	struct irq_data *idata;
-	struct irq_desc *desc;
-	struct irq_chip *chip;
-
-	if (ipd == &ipipe_root)
-		return 0;
-
-	desc = irq_to_desc(irq);
-	idata = irq_desc_get_irq_data(desc);
-	chip = irq_data_get_irq_chip(idata);
-
-	if (chip != &gpio_irqchip)
-		return -EINVAL;
-
-	at91_chip = irq_data_get_irq_chip_data(idata);
-
-	at91_chip->root &= ~(1 << idata->hwirq);
-
-	if (ipd != &ipipe_root && ++(*at91_chip->nr_nonroot) == 1)
-		return at91_chip->pioc_hwirq;
-
-	return 0;
-}
-
-int at91_gpio_disable_irqdesc(struct ipipe_domain *ipd, unsigned irq)
-{
-	struct at91_gpio_chip *at91_chip;
-	struct irq_data *idata;
-	struct irq_desc *desc;
-	struct irq_chip *chip;
-
-	if (ipd == &ipipe_root)
-		return 0;
-
-	desc = irq_to_desc(irq);
-	idata = irq_desc_get_irq_data(desc);
-	chip = irq_data_get_irq_chip(idata);
-
-	if (chip != &gpio_irqchip)
-		return -EINVAL;
-
-	at91_chip = irq_data_get_irq_chip_data(idata);
-
-	at91_chip->root |= (1 << idata->hwirq);
-
-	if (ipd != &ipipe_root && --(*at91_chip->nr_nonroot) == 0)
-		return at91_chip->pioc_hwirq;
-
-	return 0;
-}
-
-void at91_gpio_mute(void)
-{
-	struct at91_gpio_chip *prev, *chip = NULL;
-	unsigned long unmasked, muted;
-	unsigned i;
-
-	for (i = 0; i < gpio_banks; i++) {
-		prev = chip;
-		chip = gpio_chips[i];
-		if (!(*chip->nr_nonroot))
-			continue;
-
-		unmasked = __raw_readl(chip->regbase + PIO_IMR);
-		muted = unmasked & chip->root;
-		chip->muted = muted;
-		__raw_writel(muted, chip->regbase + PIO_IDR);
-	}
-}
-
-void at91_gpio_unmute(void)
-{
-	struct at91_gpio_chip *prev, *chip = NULL;
-	unsigned i;
-
-	for (i = 0; i < gpio_banks; i++) {
-		prev = chip;
-		chip = gpio_chips[i];
-		if (!(*chip->nr_nonroot))
-			continue;
-
-		__raw_writel(chip->muted, chip->regbase + PIO_IER);
-	}
-}
-#endif /* CONFIG_IPIPE */
-
 static struct platform_driver at91_gpio_driver = {
 	.driver = {
 		.name = "gpio-at91",